ebdbf78ad93ad498a0208dc74f48a5bba2433070
Felipe Pessoa <felipessoaf@hotmail.com>
2020-08-14 23:32:56 -0300

    init

---
+++ RPGTest/Assets/Scripts/JSONModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[System.Serializable]
+public class ClassesJSON{
+    public ClassModelJSON[] ClassJson;
+}
+
+[System.Serializable]
+public class ClassModelJSON{
+    public string CharName;
+    public string ClassName;
+    public string Skill;
+    public int MovementSpeed;
+}
